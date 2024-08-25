cd3e0be40f3016984c1a539ed815158317f2b0fb
kavwad <123405488+kavwad@users.noreply.github.com>
2024-08-25 11:31:37 -0700

    feat: add support for e-ink display

--- show_times.py
+++ show_times.py
@@ -7,9 +7,12 @@ Display bus times for my nearby bus stops
 import argparse
 import configparser
 import time
+import sys
 
-# import dfrobot
+sys.path.append("../DFRobot_RPi_Eink_Display")
+
 import requests
+import dfr_write
 
 import get_times
 from config import ARROWS
@@ -35,20 +38,19 @@ def get_arrows(arrivals_nearby):
     return arrivals_arrows
 
 
-def combine_6_7(arrivals_nearby):
+def combine_lines(arrivals_nearby):
     """
     Combines lines 6 and 7 into a 6/7 line in both directions, and deletes 6 and 7 lines.
     :param arrivals_nearby: custom dict with nearby bus arrivals
     :return arrivals_nearby: modified custom dict with nearby bus arrivals
     """
-    arrivals_nearby = get_arrows(arrivals_nearby)
+    # arrivals_nearby = get_arrows(arrivals_nearby)
 
-    arrivals_nearby['6/7'] = {'←': []}
+    arrivals_nearby['6/7'] = {'IB': []}
     for line in ['6', '7']:
         if line in arrivals_nearby:
-            arrivals_nearby['6/7']['←'].extend(arrivals_nearby[line].get('←', []))
-            del arrivals_nearby[line]['←']
-
+            arrivals_nearby['6/7']['IB'].extend(arrivals_nearby[line].get('IB', []))
+            del arrivals_nearby[line]['IB']
     return arrivals_nearby
 
 
@@ -60,14 +62,14 @@ def print_times(arrivals_nearby):
     :param arrivals_nearby: custom dict from parse_times function
     :return: printed list
     """
-    arrivals_nearby = combine_6_7(arrivals_nearby)
+    arrivals_nearby = combine_lines(arrivals_nearby)
 
     # Create sort_keys dict such that 6/7 gets 0 and all other lines get their int value
     sort_keys = {}
     for line in arrivals_nearby:
         if line == '6/7':
             sort_keys[line] = 0
-        elif line == 'NOWL':
+        elif line == 'NOWL' or line == 'NBUS':
             sort_keys[line] = 100
         else:
             sort_keys[line] = int(line)
@@ -76,16 +78,17 @@ def print_times(arrivals_nearby):
     arrivals_nearby_sorted = sorted(arrivals_nearby.items(),
                                     key=lambda arrival: sort_keys[arrival[0]])
 
+    to_print = []
+    
     # Print only upcoming arrivals in ascending order of line and time
     for line, arrivals in arrivals_nearby_sorted:
         for direction, times in sorted(arrivals.items()):
-            times = sorted(times)[:5] if line == '6/7' else sorted(times)[:3]
-            time_str = ', '.join(str(time) for time in times)
-            print(f'{line} {direction} in {time_str} minutes')
+            # times = sorted(times)[:5] if line == '6/7' else sorted(times)[:3]
+            times = sorted([time for time in times if time <= 30])[:3]
+            time_str = ', '.join(f'{str(time):>3}' for time in times)
+            to_print.append(f'{line:>4} {direction} in {time_str:13} minutes') if time_str else None
 
-
-def epaper(arrivals_nearby):
-    print('epaper')
+    return to_print
 
 
 def trigger_6_7(arrivals_nearby, trigger):
@@ -100,51 +103,54 @@ def trigger_6_7(arrivals_nearby, trigger):
     Next 6/7 ← in 5 minutes
     """
     # Combine 6/7 ← using helper function
-    arrivals_nearby = combine_6_7(arrivals_nearby)
+    arrivals_nearby = combine_lines(arrivals_nearby)
 
     if '6/7' not in arrivals_nearby or arrivals_nearby['6/7'] == {'←': []}:
         print('No 6/7 on the horizon.')
 
     elif trigger in arrivals_nearby['6/7']['←']:
-        # url = 'https://maker.ifttt.com/trigger/bus_soon/with/key/' + KEY_IFTT
-        url = 'https://hook.us1.make.com/55yhpa43fsgznvkc5838mhqyl7ycjte2'
-        # Send web request to Make webhook address
+        url = 'https://maker.ifttt.com/trigger/bus_soon/with/key/' + KEY_IFTT
+        # Send web request to IFTT webhook address
         response = requests.post(url)
-        print(f'6/7 in {trigger} minutes! Sent Make webhook.' if response.status_code == 200
+        print(f'6/7 in {trigger} minutes! Sent IFTTT webhook.' if response.status_code == 200
               else 'Failed to trigger the webhook.')
 
     else:
         window_min, window_max = 2, 20
         arrivals_6_7 = sorted(arrivals_nearby['6/7']['←'])
         next_6_7 = [str(n) for n in arrivals_6_7 if window_min < n < window_max]
-        print(f'Next 6/7 ← in {', '.join(next_6_7)} minutes' if next_6_7
-              else f'No 6/7 in the next {window_max} minutes.')
+        print(f'Next 6/7 ← in {", ".join(next_6_7)} minutes' if next_6_7 else f'No 6/7 in the next {window_max} minutes.')
 
 
 def main():
     parser = argparse.ArgumentParser(description="Get nearby bus times")
     parser.add_argument('-print', action='store_true',
                         help='print bus times')
-    parser.add_argument('-epaper', action='store_true',
+    parser.add_argument('-display', action='store_true',
                         help='display output on e-paper')
     parser.add_argument('-trig', nargs=2, type=int,
                         help='[requires two ints] trigger IFTT when 6/7 {int1} mins away, every min for {int2} mins')
     parser.add_argument('-shelve', action='store_true',
                         help='get and shelve API data')
     parser.add_argument('-dev', action='store_true',
-                        help='[opt for -print, -trigger, -epaper] use shelved data')
+                        help='[opt for -print, -trigger, -display] use shelved data')
 
     args = parser.parse_args()
 
     if args.print:
         all_data = get_times.call_511('SF') if not args.dev else get_times.get_from_shelf()
         arrivals_nearby = get_times.parse_times(all_data)
-        print_times(arrivals_nearby)
+        to_print = print_times(arrivals_nearby)
+        for line in to_print:
+            print(line)
 
-    if args.epaper:
+    if args.display:
         all_data = get_times.call_511('SF') if not args.dev else get_times.get_from_shelf()
         arrivals_nearby = get_times.parse_times(all_data)
-        epaper(arrivals_nearby)
+        to_print = print_times(arrivals_nearby)
+        # for line in to_print: #debug
+        #     print(line)
+        dfr_write.write(to_print)
 
     if args.trig:
         trigger, loops = args.trig
